a3f9729703a67dd9b8e35d6d190bcdcc8a3106d4
FTinCNS <tjdrhr02@naver.com>
2024-12-06 15:19:57 +0900

    [test] git test

---
+++ src/main/java/com/ft/first_spring/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.ft.first_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data","hello!!");
+        return "hello";
+    }
+
+    static class myClass {
+        String name;
+
+
+
+    }
+}
